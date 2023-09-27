830ad5ee835384cfa636e84d6b2b890bb7c773bd
Valentin Karakashev <116033694+ValchoPalcho@users.noreply.github.com>
2023-09-27 13:13:07 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Project___Guess_The_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
