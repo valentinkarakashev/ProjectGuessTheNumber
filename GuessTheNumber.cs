d7e9c2c349efd4b4cc1e4b18031f3a63b2c5a46d
Valentin Karakashev <116033694+ValchoPalcho@users.noreply.github.com>
2023-09-27 13:41:07 +0300

    I changed it!

---
+++ GuessTheNumber.cs
@@ -0,0 +1,39 @@
+﻿namespace Project___Guess_The_Number
+{
+    internal class GuessTheNumber
+    {
+        static void Main(string[] args)
+        {
+            Random randomNumber = new Random();
+            int computerNumber = randomNumber.Next(1, 101);
+
+            while (true)
+            {
+                Console.Write("Guess a number (1 - 100): ");
+                string playerInput = Console.ReadLine();
+                bool isValid = int.TryParse(playerInput, out int playerNumber);
+
+                if (isValid) 
+                {
+                    if (playerNumber == computerNumber) 
+                    {
+                        Console.WriteLine("You guessed it!");
+                        break;
+                    }
+                    else if(playerNumber > computerNumber)
+                    {
+                        Console.WriteLine("Too High");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Too Low");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("The Input is Invalid!");
+                }
+            }
+        }
+    }
+}
